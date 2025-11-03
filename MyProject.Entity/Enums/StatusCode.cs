7b0a3e235e22b43e098c2e3746f18bb79e217725
Elifnaz00 <demirelifnaz9@gmail.com>
2025-11-03 20:01:05 +0300

    Api durum kodları için enum oluşturuldu

---
+++ MyProject.Entity/Enums/StatusCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace MyProject.Entity.Enums
+{
+    public enum StatusCode
+    {
+        Ok = 200,
+        Created = 201,
+        NoContent= 204,
+        BadRequest = 400,
+        Unauthorized = 401,
+        NotFound = 404,
+        InternalServerError = 500
+    }
+}
